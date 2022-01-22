52756c92cbeb6a47fe24fa148c2c217af0efc099
Rick van Wijk <404350@student.fontys.nl>
2022-01-22 13:09:54 +0100

    Updates

--- _nuxt/ceca234.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{218:function(n,e,t){"use strict";t.r(e);var c={},o=t(36),component=Object(o.a)(c,(function(){var n=this,e=n.$createElement;return(n._self._c||e)("div",{},[n._v("\n  About \n")])}),[],!1,null,"0f4db836",null);e.default=component.exports}}]);
---
+++ _nuxt/static/1642853383/about/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Frvwijk_portfolio\u002F_nuxt\u002Fstatic\u002F1642853383",layout:"default",error:a,serverRendered:true,routePath:"\u002Fabout",config:{_app:{basePath:"\u002Frvwijk_portfolio\u002F",assetsPath:"\u002Frvwijk_portfolio\u002F_nuxt\u002F",cdnURL:a}}}}(null));

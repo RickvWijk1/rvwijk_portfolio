f543f5e9f814fd3f6e02bf7a847738ec9a38b5cd
Rick van Wijk <404350@student.fontys.nl>
2022-01-24 12:39:56 +0100

    Updates

--- _nuxt/static/1643023798/interests/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Frvwijk_portfolio\u002F_nuxt\u002Fstatic\u002F1643023798",layout:"default",error:a,serverRendered:true,routePath:"\u002Finterests",config:{_app:{basePath:"\u002Frvwijk_portfolio\u002F",assetsPath:"\u002Frvwijk_portfolio\u002F_nuxt\u002F",cdnURL:a}}}}(null));

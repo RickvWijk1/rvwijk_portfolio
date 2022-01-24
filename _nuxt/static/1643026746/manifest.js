6684454d0ea615e6e9757688054125c15966df61
Rick van Wijk <404350@student.fontys.nl>
2022-01-24 13:19:27 +0100

    Updates

---
+++ _nuxt/static/1643026746/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fprojects","\u002Finterests","\u002Fabout"]})
---
+++ _nuxt/static/1643026746/projects/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Frvwijk_portfolio\u002F_nuxt\u002Fstatic\u002F1643026746",layout:"default",error:a,serverRendered:true,routePath:"\u002Fprojects",config:{_app:{basePath:"\u002Frvwijk_portfolio\u002F",assetsPath:"\u002Frvwijk_portfolio\u002F_nuxt\u002F",cdnURL:a}}}}(null));

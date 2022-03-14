e829d4bd5af8c3fcb109e3bea95d38e468c8da37
Rick van Wijk <404350@student.fontys.nl>
2022-03-14 20:01:13 +0100

    Updates

--- _nuxt/54fd163.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[10],{301:function(e,n,t){"use strict";t.r(n);var c={},o=t(25),component=Object(o.a)(c,(function(){var e=this.$createElement;return(this._self._c||e)("section")}),[],!1,null,"03b57e2c",null);n.default=component.exports}}]);
---
+++ _nuxt/ef6ed5c.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[17],{350:function(t,e,n){"use strict";n.r(e);var l={},r=n(32),component=Object(r.a)(l,(function(){var t=this.$createElement;return(this._self._c||t)("div",{staticClass:"wrapper"})}),[],!1,null,null,null);e.default=component.exports}}]);

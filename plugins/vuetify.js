1d986b3a2740da75a7f2d19bf682a34a7c23e3dd
rvanwijk <rickvanwijk@bluebirdday.nl>
2022-11-03 09:18:41 +0100

    added content

--- plugins/vuetify.js
+++ plugins/vuetify.js
@@ -4,12 +4,12 @@ import * as components from 'vuetify/components'
 export default defineNuxtPlugin(nuxtApp => {
     const vuetify = createVuetify({
         components,
-        theme: { 
-            disable: true 
-        }
+        // theme: {
+        //     disable: true
+        // }
+        theme: { light: true },
     })
 
     nuxtApp.vueApp.use(vuetify)
   })
 
-  
